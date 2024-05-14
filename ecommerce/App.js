cfe4d18fb21f24ea0f76706e04b566119958e426
VirLustitiae <zuvanovm@gmail.com>
2024-05-14 18:22:28 -0300

    projeto final

--- ecommerce/App.js
+++ ecommerce/App.js
@@ -1,44 +1,15 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SplashScreen from './screens/SplashScreen';
-import LoginScreen from './screens/LoginScreen';
-import MainScreen from './screens/MainScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import AddProductScreen from './screens/AddProductScreen'; // Certifique-se de que o caminho está correto
+import 'react-native-gesture-handler';
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
-const Stack = createNativeStackNavigator();
-
+import { SecurityContext } from "./context/securityContext";
+import { Routes } from "./appRoutes";
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash">
-        <Stack.Screen
-          name="Splash"
-          component={SplashScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Main"
-          component={MainScreen} 
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Register"
-          component={RegisterScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="AddProduct"
-          component={AddProductScreen}
-          options={{ title: 'Adicionar Produto', headerShown: true }} 
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SecurityContext>
+      <NavigationContainer>
+        <Routes/>
+      </NavigationContainer>
+    </SecurityContext>
   );
 }
--- ecommerce/firebase/api.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: "https://ecommerce-c66db-default-rtdb.firebaseio.com",
-});
